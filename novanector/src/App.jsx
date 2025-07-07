0616f94a1ad4aabee8f7ff2171bafebde677e9e5
Piyush Bidaliya <piyushbidaliya@gmail.com>
2025-07-07 10:52:59 +0530

    first commit

---
+++ novanector/src/App.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom"
+import Header from "./component/Header"
+import Footer from "./component/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
